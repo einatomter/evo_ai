a2a5c48f2a5acc5e140d194cc526d74230928fc6
einar <einatomter@gmail.com>
2022-09-29 16:26:59 +0200

    added rng-based rules for cartpole
    Co-authored-by: erikskul <erikskul@users.noreply.github.com>

--- cartpole_sample.py
+++ cartpole_sample.py
@@ -2,7 +2,7 @@
 
 import gym
 import numpy as np
-from time import sleep
+from evo_ai import *
 
 env = gym.make("CartPole-v1", render_mode="human")
 observation, info = env.reset(seed=42)
@@ -10,29 +10,35 @@ observation, info = env.reset(seed=42)
 angle = 0
 action = 1
 
+time = 0
+
+ruleset = generate_rrs(3)
+print(f'ruleset:\t{ruleset}')
+
 for _ in range(1000):
     # action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
     
     angle = observation[2]
-    # if angle < 0:
-    #     action = 0
-    # else:
-    #     action = 1
+
+    bin_angle = format(24 + round(angle * 180/np.pi), "b")
+
+    bin_angle = bin_angle.zfill(6)
+
+    action = ca_output(cellular_automaton(ruleset, bin_angle))
 
     # print(observation)
     # print(reward)
 
-    # convert observation states to binary forms with resolution n
-    # print(24 + round(angle * 180/np.pi))
-
-    # offset starts from min observable angle (-24)
-    bin_angle = format(24 + round(angle * 180/np.pi), "b")
-
-    bin_angle = bin_angle.zfill(6)
-    
-    print(bin_angle)
+    time += reward
     
     if terminated or truncated:
+        if time > 30:
+            print(f'ruleset:\t{ruleset}')
+            print(f'time: {time}')
+            
+        ruleset = generate_rrs(3)
+        
+        time = 0
         observation, info = env.reset()
 env.close()
--- evo_ai.py
+++ evo_ai.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 
+import gym
+import numpy as np
+import random
+
 # TODO: Rename variables to match terminology from lectures
 
 
 
-def cellular_automaton(ruleset, initial_values):
+def cellular_automaton(ruleset: str, radius, initial_values: str):
     '''
-        performs update step
+        Performs update step
 
-        ruleset is an array of possible rules
+        Ruleset is an array of possible rules
 
-        initial values is binary string from observations
+        Initial values is binary string from observations
 
-        TODO: add parameter for substr size (should be odd value?)
+        TODO: Add parameter for substr size (should be odd value?)
     '''
 
     # variable declaration
@@ -20,41 +24,65 @@ def cellular_automaton(ruleset, initial_values):
 
 
     print(f'initial values:\t{initial_values}')
+    print(f'ruleset:\t{ruleset}')
+    
 
     # TODO: range based on size of initial_values
     # range is size of cellular automaton
     for i in range(6):
         substr = ""
-        substr = substr + initial_values[i-1] + initial_values[i] + initial_values[(i+1)%6]
-        # print(substr)
+        for cell in range(radius, (radius+1)):
+            substr += initial_values[(i + cell) % 6]
 
         # convert substr = decimal
         substr_10 = int(substr, 2)
 
-        # check for match in ruleset and save resulting value to new grid
-        if substr_10 in ruleset:
-            new_values = new_values + "1"
-            # print("found match")
-        else:
-            new_values = new_values + "0"
+        new_values += ruleset[substr_10]
     # /loop
 
     print(f'new values:\t{new_values}')
     
-    # return new_values
+    return new_values
 
-# def ca_output(new_val):
+def ca_output(bitstring: str) -> bool:
+    '''
+        Determines action of the cart.
+        Currently decides based on majority vote.
 
-# if more 1s, go right, else go left
+        0 = left, 1 = right
+    '''
+    
+    ones = bitstring.count('1')
+    zeros = bitstring.count('0')
+
+    if ones >= zeros:
+        return 1
+    else:
+        return 0
 
 
 
+# Generate random ruleset
+# returns bitstring
+def generate_rrs(subst_size):
+    ''' 
+        Generates a random ruleset based on the amount of neighbours.
+        
+        Input: size of substring (int)
+        Returns: random bitsting within range (str)
+    '''
 
+    max_substrings = 2 ** subst_size
+    max_rules = (2 ** max_substrings) - 1 # max 255
+    
+    rrs_dec = random.randrange(0, max_rules) # 140   10001100
+    rrs_bit = format(rrs_dec, "b").zfill(max_substrings)
+
+    return rrs_bit
 
 
 def main():
-    cellular_automaton([5], "101110")
-
+    cellular_automaton(generate_rrs(5), 2, "101110")
 
 
 
