b4471ca61cf7ee7b9ea5b3e841dd097421908fb2
einar tomter <einatomter@gmail.com>
2022-10-20 09:44:16 +0200

    merge main with einar

--- cellular_automata.py
+++ cellular_automata.py
@@ -17,7 +17,6 @@ from yaml.loader import SafeLoader
 class CA:
     def __init__(self) -> None:
         # Config stuff
-<<<<<<< HEAD
         self._MAXPOP = 100                  # 100 maximum amount of rules of the first random population
         self._RADIUS = 2                    # 2 neighbours = radius*2
         self._ENABLE_THRESHOLD = False       # random search with (False) or without threshold (True)
@@ -37,15 +36,6 @@ class CA:
         self.max_angle = 0.2095
         self.min_ang_velocity = -2
         self.max_ang_velocity = 2
-=======
-        self._MAXPOP = 100 #100 maximum amount of rules of the first random population
-        self._RADIUS = 2 #2  neighbours = radius*2
-        self._ENABLE_THRESHOLD = False # random search with (False) or without threshold (True)
-        self._RANDOM_THRESHOLD_SIZE = 30
-        self._ENABLE_SEED = True
-        self._SEED = 42 #42 seed for initial env.reset()
-        self._TESTS = 1 # how many times to test evolved rules before evolving again
->>>>>>> main
 
         # env = gym.make("CartPole-v1", render_mode="human")
         self.env = gym.make("CartPole-v1")
